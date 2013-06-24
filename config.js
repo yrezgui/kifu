a866175c9355682ba9dcaf07be4741e2a7f32adb
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-24 21:53:34 +0200

    Add Stripe currency setting

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 var config = require('config-env').define('NODE_ENV', function (config) {
 	config.common({
 		name: 'jolicoeur',
+		// Title
+		title: 'JoliCoeur',
 		// Express port
 		express_port: 3005,
 	});
@@ -8,8 +10,10 @@ var config = require('config-env').define('NODE_ENV', function (config) {
 	config.config('development', {
 		// Stripe Private Key
 		stripe_privateKey: 'SSSSSSSSSSSSSSSSSSSS',
+		// Stripe Public Key
+		stripe_publicKey: 'PPPPPPPPPPPPPPPPPPPP',
 		// Stripe Default Currency
-		stripe_currency: 'usd'
+		stripe_currency: 'usd',
 	});
 });
 
