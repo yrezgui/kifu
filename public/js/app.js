03c5165e328d88bacbf944ec73cfba26c3344f9a
Yacine Rezgui <rezgui.y@gmail.com>
2013-06-26 00:13:10 +0200

    Add sending of stripe request to the server

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,39 @@
-var getPaymentToken = function getPaymentToken(token){
-  console.log(token);
+var getPaymentToken = function getPaymentToken(token, amount){
+
+  var formObject = new FormData();  
+  formObject.append('stripe_token', token.id);  
+  formObject.append('amount', amount);  
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function onreadystatechange() {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        console.log(xhr.responseText);
+      }
+      else {
+        console.log('There was a problem with the request.');
+      }
+    }
+  };
+
+  xhr.open('POST', '/donate');
+  xhr.send(formObject);
 };
 
 var donateClick = function donateClick(event, b, c) {
-  console.log(this, this.dataset.amount, this.dataset, event, b, c);
-  //elem.dataset;
+  var elem = this;
+  
+  // Open Modal Stripe payment
   StripeCheckout.open({
     key:         STRIPE_PUBLIC_KEY,
-    amount:      this.dataset.amount + '00',
+    amount:      elem.dataset.amount + '00',
     currency:    STRIPE_CURRENCY,
-    name:        'Donation of ' + this.dataset.amount + '$',
-    description: this.dataset.description,
+    name:        'Donation of ' + elem.dataset.amount + '$',
+    description: elem.dataset.description,
     panelLabel:  'Donate',
-    token:       getPaymentToken
+    token:       function(token) {
+      getPaymentToken(token, elem.dataset.amount + '00');
+    }
   });
 
   return false;
